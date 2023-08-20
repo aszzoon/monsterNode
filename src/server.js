e1b67495ad386af2087920c5f32ccf09a293b607
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2023-08-21 03:31:53 +0900

    feat: clean code

--- src/server.js
+++ src/server.js
@@ -1,35 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+
 const PORT = 4000;
-
 const app = express();
-const logger = morgan("dev");
 
+// Middleware
+const logger = morgan("dev");
 app.use(logger);
 
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
-
+// Mount Routers
 app.use("/", globalRouter);
-app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
+// Server Start
 const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} `);
 
-  app.listen(PORT, handleListening);
+app.listen(PORT, handleListening);

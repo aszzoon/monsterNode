1800d79e526c7bf426436c11be182624b31ccd8c
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2023-08-21 04:00:06 +0900

    feat: Controller

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
-# monsternode Reloaded
+# 게시판 🍀
 
-/ -> Home
-/join -> Join
-/login -> Login
-/search -> Search
+***
 
-/users/edit -> Edit user
-/users/delete -> Delete user
+node.js
+express
+mongodb
+mongoose
 
-/videos/watch -> Watch Video
-/videos/edit -> Edit Video
-/videos/delete -> Delete Video
-/videos/comments -> Comment on a video
-/videos/comments/delete -> Delete A Comment of a Video
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
+import {edit, remove} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleEditUser = (req, res) => res.send("Edit User");
-const handleDelete = (req, res) => res.send("Delete User");
-
-userRouter.get("/edit", handleEditUser);
-userRouter.get("/delete", handleDelete);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 export default userRouter;

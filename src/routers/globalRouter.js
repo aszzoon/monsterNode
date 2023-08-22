c5499c8e5eda9c8587bd5085f06714018a33a536
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2023-08-23 01:54:59 +0900

    feat: routes

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {join} from "../controllers/userController.js";
-import {trending} from "../controllers/videoController.js";
+import {join, login} from "../controllers/userController.js";
+import {trending, search} from "../controllers/videoController.js";
 
 const globalRouter = express.Router();
 
@@ -9,5 +9,7 @@ const handleHome = (req, res) => res.send("Home");
 globalRouter.get("/", handleHome);
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
